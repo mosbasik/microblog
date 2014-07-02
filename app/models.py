953a5d308c0fb874aa5bbfe619de33bd77d538cd
Peter Henry <phenryextra@zoho.com>
2014-07-02 14:14:18 -0500

    "More Interesting Profiles 1"

--- app/models.py
+++ app/models.py
@@ -6,11 +6,13 @@ ROLE_ADMIN = 1
 
 
 class User(db.Model):
-    id       = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email    = db.Column(db.String(120), index=True, unique=True)
-    role     = db.Column(db.SmallInteger, default=ROLE_USER)
-    posts    = db.relationship('Post', backref='author', lazy='dynamic')
+    id        = db.Column(db.Integer, primary_key=True)
+    nickname  = db.Column(db.String(64), index=True, unique=True)
+    email     = db.Column(db.String(120), index=True, unique=True)
+    role      = db.Column(db.SmallInteger, default=ROLE_USER)
+    posts     = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me  = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     def is_authenticated(self):
         '''
