1228ff45790c11faafc588abdd973c0d2ad679e1
Peter Henry <phenryextra@zoho.com>
2014-07-02 02:20:01 -0500

    "Configuration"

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,16 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+
+
+#-------------------------------
+# User logins
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
