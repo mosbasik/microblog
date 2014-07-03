2f2de4110c8acbe18a01c7a0733b02cdaaa34d19
Peter Henry <phenryextra@zoho.com>
2014-07-03 07:16:22 -0500

    "Database Model"

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ ROLE_USER = 0
 ROLE_ADMIN = 1
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user_id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user_id'))
+)
+
+
 class User(db.Model):
     id        = db.Column(db.Integer, primary_key=True)
     nickname  = db.Column(db.String(64), index=True, unique=True)
@@ -13,6 +19,13 @@ class User(db.Model):
     posts     = db.relationship('Post', backref='author', lazy='dynamic')
     about_me  = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed  = db.relationship('User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     def is_authenticated(self):
         '''
